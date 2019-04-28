bbeb12dbca20d41a3e9b16cbfafa51a3f4695975
Trevor Fehrman <trevorfehrman@gmail.com>
2019-04-27 18:33:58 -0700

    vanilla api in place

--- src/index.js
+++ src/index.js
@@ -6,14 +6,27 @@ let links = [
     id: 'link-0',
     url: 'www.howtographql.com',
     description: 'Fullstack tutorial for GraphQL'
+  },
+  {
+    id: 'link-1',
+    url: 'boop',
+    description: 'hider'
   }
 ];
+
 let idCount = links.length;
+
+
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links
+    feed: () => links,
+    link: (parent, args) => {
+      result = links.filter(link => link.id == args.id)[0];
+      return result;
+    }
   },
+
   Mutation: {
     // 2
     post: (parent, args) => {
@@ -24,6 +37,42 @@ const resolvers = {
       };
       links.push(link);
       return link;
+    },
+    
+    updateLink: (parent, args) => {
+      let found;
+      newLinks = links.map(link => {
+        if (link.id == args.id) {
+          found = {
+            ...link,
+            id: args.id,
+            url: args.url || link.url,
+            description: args.description || link.description
+          };
+
+          return found;
+        } else {
+          return link;
+        }
+      });
+      links = newLinks;
+
+      return found;
+    },
+
+    deleteLink: (parent, args) => {
+      let found;
+      newLinks = links.filter(link => {
+        if (link.id != args.id) {
+          return link;
+        }
+
+        if (link.id == args.id) {
+          found = link;
+        }
+      });
+      links = newLinks;
+      return found;
     }
   }
 };
